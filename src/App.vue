6541b529c5cee186695d3473c238393660705942
Dens26 <denis.bekaert@live.fr>
2024-03-24 00:29:16 +0100

    📦 NEW: Premiere version du navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Accueil</RouterLink>
-      <RouterLink to="/a-propos">A propos</RouterLink>
-    </nav>
+
   </header>
 
   <RouterView />
