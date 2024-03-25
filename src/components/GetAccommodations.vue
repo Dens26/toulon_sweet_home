693921063de2697f1a47f7bd2e5f4212b661ce78
Dens26 <denis.bekaert@live.fr>
2024-03-25 15:46:55 +0100

    👌 IMPROVE: page d'accueil

--- src/components/GetAccommodations.vue
+++ src/components/GetAccommodations.vue
@@ -11,8 +11,27 @@ console.log(accommodations)
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
             <div class="col" v-for="accommodation in accommodations" :key="accommodation.id">
                 <div class="card h-100 border-0">
-                    <img v-bind:src="accommodation.pictures[0].file" class="card-img-top img-fluid img-square"
-                        v-bind:alt="'photo ' + accommodation.name">
+
+                    <div v-bind:id="'carousel-' + accommodation.id" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div v-for="(picture, index) in accommodation.pictures" :key="index" class="carousel-item" v-bind:class="{'active' : index === 0}">
+                                <img v-bind:src="picture.file_small"
+                                    class="d-block w-100 img-fluid img-square"
+                                    v-bind:alt="'photo ' + accommodation.name">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" v-bind:data-bs-target="'#carousel-' + accommodation.id"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" v-bind:data-bs-target="'#carousel-' + accommodation.id"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
                     <div class="card-body d-flex flex-column justifiy-content-between">
                         <h5 class="card-title h5">{{ accommodation.name.substring(0, 50) }}</h5>
                         <p class="card-text fs-6">{{ accommodation.description.substring(0, 50) + '...' }}</p>
@@ -20,6 +39,7 @@ console.log(accommodations)
                             <p class="card-text"><span class="h5">{{ accommodation.price + " €" }}</span> par nuit</p>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -27,7 +47,6 @@ console.log(accommodations)
 </template>
 
 <style scoped>
-
 .img-square {
     object-fit: cover;
     aspect-ratio: 50/50;
